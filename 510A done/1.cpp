e44472c8271d65743779c200dd28f901b1665c84
samansayad93 <94793129+samansayad93@users.noreply.github.com>
2023-07-30 19:43:40 +0330

    230A done 4C done 510A done

---
+++ 510A done/1.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+
+int main(){
+
+    int n, m;
+    std::cin>>n>>m;
+
+    for(int p = 0; p < n; p++){
+        if(p % 2 == 0){for(int k = 0; k < m; k++){printf("#");}; puts("");}
+        else if(p % 4 == 1){for(int k = 0; k < m - 1; k++){printf(".");}; puts("#");} 
+        else if(p % 4 == 3){printf("#"); for(int k = 0; k < m - 1; k++){printf(".");}; puts("");} 
+    }
+
+    return 0;
+}
